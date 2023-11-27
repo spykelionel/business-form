4c19f2594e17b70b002138e2d31bc9ee88dd5b55
spykelionel <social.lionel@gmail.com>
2023-11-27 04:11:39 +0100

    finish updates

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ import {
   Typography,
   Button,
   TextField,
+  Tooltip,
+  IconButton,
+  OutlinedInput,
+  InputAdornment,
+  MenuItem,
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import cat from "./cat.jpeg";
-import { red } from "@mui/material/colors";
+import {
+  Cancel,
+  CancelOutlined,
+  Check,
+  CloseSharp,
+  QuestionMarkOutlined,
+  Visibility,
+  VisibilityOff,
+} from "@mui/icons-material";
 
 const theme = createTheme({
   components: {
@@ -423,14 +436,35 @@ const BusinessForm = ({
   byValue,
 }) => {
   const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    phone: "",
+    brandName: "",
+    brandType: "",
+    streetAddress: "",
+    city: "",
+    zipCode: "",
+    taxIdNumber: "",
   });
 
+  const tooltipTitle = `
+  Local: Brands with distribution in 3 divisions or less 
+  OR multiple divisions or a total of 50 stores or less.\n\n
+
+  National: Brands with distributions with in 4 or more 
+  divisions or in more than 150 stores.
+  `;
+  const brands = [
+    {
+      value: "Apple",
+      label: "Apple",
+    },
+    {
+      value: "Google",
+      label: "Google",
+    },
+    {
+      value: "BTC",
+      label: "฿",
+    },
+  ];
   const handleInputChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
   return (
@@ -449,83 +483,291 @@ const BusinessForm = ({
           title={"Business Information"}
           description="Please enter information about your company"
         />
+        <Typography>
+          <p className="text-info">General Information</p>
+        </Typography>
         <form onSubmit={(e) => e.preventDefault()}>
           <Grid container spacing={2}>
             <Grid item xs={12} md={6}>
               <Box my={1}>
-                <label htmlFor="firstName">First Name*</label>
+                <label htmlFor="brandName">Brand Name*</label>
               </Box>
               <TextField
                 fullWidth
                 placeholder="Input your first name"
-                name="firstName"
-                value={formData.firstName}
+                name="brandName"
+                value={formData.brandName}
                 onChange={handleInputChange}
               />
             </Grid>
             <Grid item xs={12} md={6}>
               <Box my={1}>
-                <label htmlFor="lastName">Last Name*</label>
+                <label htmlFor="brandType">Brand Type*</label>
+                <Tooltip
+                  title={tooltipTitle}
+                  arrow
+                  style={{ position: "relative" }}
+                >
+                  <IconButton size="small">
+                    <QuestionMarkOutlined
+                      fontSize="7px"
+                      style={{
+                        position: "absolute",
+                        top: "-5px",
+                        left: "10px",
+                        color: "#fff",
+                        backgroundColor: "grey",
+                        borderRadius: "50%",
+                        padding: "2px",
+                      }}
+                    />
+                  </IconButton>
+                </Tooltip>
               </Box>
               <TextField
                 fullWidth
-                placeholder="Input your last name"
-                name="lastName"
-                value={formData.lastName}
+                id="outlined-select-currency"
+                select
+                defaultValue="Apple"
+              >
+                {brands.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <Box my={1}>
+                <label htmlFor="streetAddress">Street Address*</label>
+              </Box>
+              <TextField
+                fullWidth
+                placeholder="Input your Street Address"
+                name="streetAddress"
+                value={formData.streetAddress}
                 onChange={handleInputChange}
               />
             </Grid>
 
             <Grid item xs={12} md={6}>
               <Box my={1}>
-                <label htmlFor="email">Email*</label>
-              </Box>
-              <TextField
-                fullWidth
-                placeholder="Input your email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <Box my={1}>
-                <label htmlFor="phone">Phone Number*</label>
+                <label htmlFor="phone">City *</label>
               </Box>
               <TextField
                 type="tel"
                 fullWidth
-                placeholder="Input your phone number"
-                name="phone"
+                placeholder="Input your City"
+                name="city"
                 value={formData.phone}
                 onChange={handleInputChange}
               />
             </Grid>
             <Grid item xs={12} md={6}>
               <Box my={1}>
-                <label htmlFor="password">Password*</label>
+                <label htmlFor="zipCode">Zip Code*</label>
               </Box>
               <TextField
                 fullWidth
-                placeholder="Input your password"
-                name="password"
-                value={formData.password}
+                placeholder="Input your Zip code"
+                name="zipCode"
+                value={formData.zipCode}
                 onChange={handleInputChange}
               />
             </Grid>
 
             <Grid item xs={12} md={6}>
               <Box my={1}>
-                <label htmlFor="confirmPassword">Repeat Password*</label>
+                <label htmlFor="taxIdNumber">Tax ID Number*</label>
               </Box>
               <TextField
                 fullWidth
-                placeholder="Confirm your password"
-                name="confirmPassword"
-                value={formData.confirmPassword}
+                placeholder="Input your Tax ID Number"
+                name="taxIdNumber"
+                value={formData.taxIdNumber}
                 onChange={handleInputChange}
               />
+            </Grid>
+          </Grid>
+          <Grid>
+            <Typography>
+              <p className="text-info">Documents</p>
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item md={12} mt={1}>
+                <label htmlFor="brandName">
+                  Once the following Documents are signed you will be able to
+                  get started
+                </label>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={12}
+                width={100}
+                display={"flex"}
+                justifyContent="space-between"
+                container
+                spacing={2}
+              >
+                <Grid item md={10}>
+                  <OutlinedInput
+                    fullWidth
+                    id="outlined-adornment-password"
+                    // disabled
+                    value="Electronically signed the agreement(s)"
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton aria-label="">
+                          <Check
+                            style={{ color: "green", fontWeight: "bold" }}
+                          />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </Grid>
+
+                <Grid
+                  item
+                  md={2}
+                  container
+                  justifyContent={"flex-end"}
+                  style={{ height: "100%" }}
+                >
+                  <Button
+                    type="button"
+                    variant="contained"
+                    // disabled={step === 3}
+                    style={{
+                      textTransform: "none",
+                      backgroundColor: "#261eb6",
+                      height: "100%",
+                      fontWeight: "bold",
+                    }}
+                    // onClick={handleNextButtonClicked}
+                  >
+                    &gt;
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <Grid
+                item
+                xs={12}
+                md={12}
+                width={"100%"}
+                display={"flex"}
+                justifyContent="space-between"
+                container
+                spacing={2}
+              >
+                <Grid item md={10}>
+                  <OutlinedInput
+                    fullWidth
+                    id="outlined-adornment-password"
+                    // disabled
+                    value="No Adult Beverage Kroger market supplier waiver and release"
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton aria-label="">
+                          <CloseSharp
+                            style={{ color: "red", fontWeight: "bold" }}
+                          />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </Grid>
+
+                <Grid
+                  item
+                  md={2}
+                  style={{ height: "100%" }}
+                  justifyContent={"flex-end"}
+                  container
+                >
+                  <Button
+                    type="button"
+                    variant="contained"
+                    // disabled={step === 3}
+                    style={{
+                      textTransform: "none",
+                      backgroundColor: "#261eb6",
+                      height: "100%",
+                      fontWeight: "bold",
+                    }}
+                    // onClick={handleNextButtonClicked}
+                  >
+                    &gt;
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Typography>
+              <p className="text-info">COI PDF Upload</p>
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item md={12} mt={1}>
+                <label htmlFor="brandName">
+                  Once the following Documents are signed you will be able to
+                  get started
+                </label>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={12}
+                width={100}
+                display={"flex"}
+                justifyContent="space-between"
+                container
+                spacing={2}
+              >
+                <Grid item md={10}>
+                  <OutlinedInput
+                    fullWidth
+                    id="outlined-adornment-password"
+                    // disabled
+                    value="Electronically signed the agreement(s)"
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton aria-label="">
+                          <Check
+                            style={{ color: "green", fontWeight: "bold" }}
+                          />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </Grid>
+
+                <Grid
+                  item
+                  md={2}
+                  container
+                  justifyContent={"flex-end"}
+                  style={{ height: "100%" }}
+                >
+                  <Button
+                    type="button"
+                    variant="contained"
+                    // disabled={step === 3}
+                    style={{
+                      textTransform: "none",
+                      backgroundColor: "#261eb6",
+                      height: "100%",
+                      fontWeight: "bold",
+                    }}
+                    // onClick={handleNextButtonClicked}
+                  >
+                    &gt;
+                  </Button>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </form>
