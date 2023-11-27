4a59f7f58fe989dbc156a4e82f9ba4ab011c5135
spykelionel <social.lionel@gmail.com>
2023-11-27 04:24:38 +0100

    Add AppContext

---
+++ src/AppContext.jsx
@@ -0,0 +1,35 @@
+// AppContext.js
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [step, setStep] = useState(1);
+  const [completedSteps, setCompletedSteps] = useState({
+    one: true,
+    two: false,
+    three: false,
+  });
+  const byValue = 100 / 3;
+  const [percentageCompleted, setPercentageCompleted] = useState(byValue);
+
+  return (
+    <AppContext.Provider
+      value={{
+        step,
+        setStep,
+        completedSteps,
+        setCompletedSteps,
+        percentageCompleted,
+        setPercentageCompleted,
+        byValue,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
