2c9295abb702c971e075830c19400575d6149643
spykelionel <social.lionel@gmail.com>
2023-11-27 05:56:24 +0100

    define proptypes for Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import useAppContext from "../hooks/useAppContext";
 import { Container, CssBaseline } from "@mui/material";
 import MiniHeader from "./MiniHeader";
 import MiniFooter from "./MiniFooter";
 
+/**
+ * Layout component for consistent structure.
+ * @param {React.ReactNode} props.children - The content to be rendered within the layout.
+ * @returns {React.ReactNode} - Rendered layout component.
+ */
 const Layout = ({ children }) => {
   const { step, setStep, setCompletedSteps, setPercentageCompleted, byValue } =
     useAppContext();
@@ -24,6 +30,10 @@ const Layout = ({ children }) => {
       </Container>
     </Container>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
